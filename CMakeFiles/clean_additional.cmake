08a563075d236054628e996dee7e9c98c631bca8
Igor-Szab <szablewskiigor@gmail.com>
2026-06-01 10:11:21 +0200

    feat/one const code for puzzle 3

--- CMakeFiles/clean_additional.cmake
+++
@@ -1,9 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "debug")
-  file(REMOVE_RECURSE
-  "output.map"
-  "program.bin"
-  )
-endif()
